f5444327969b4091c5f2504b96e0e92e97faae85
laura <lauraliu1117@gmail.com>
2022-06-16 09:38:00 -0700

    Initial commit

---
+++ question_model.py
@@ -0,0 +1,11 @@
+# class Question:  #create question class from scratch
+#
+#     def __int__(self, question_text, question_answer):
+#         self.text = question_text
+#         self.answer = question_answer
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
